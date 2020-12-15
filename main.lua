0007657a9d2c27ebe0f505256cb3d2b57e16a686
axorkgamecodeur <axorkgamecodeur@gmail.com>
2020-12-15 19:05:17 +0100

    Ajout du déplacement

--- main.lua
+++ main.lua
@@ -9,16 +9,31 @@ love.graphics.setDefaultFilter("nearest")
 if arg[#arg] == "-debug" then require("mobdebug").start() end
 
 local image
+local largeur
+local hauteur
+local imageX
+local imageY
+local vitesse
 
 function love.load()
     largeur = love.graphics.getWidth()
     hauteur = love.graphics.getHeight()
 
+    imageX = 100
+    imageY = 100
+
+    vitesse = 3
+
     image = love.graphics.newImage("images/heros.png")
 end
   
 function love.update(dt)
-  
+    imageX = imageX + vitesse
+    if imageX > largeur - image:getWidth()/2 and vitesse > 0 then
+        vitesse = -4
+    elseif imageX < 0 and vitesse < 0 then
+        vitesse = 4
+    end
 end
   
 function love.draw()
@@ -29,7 +44,8 @@ function love.draw()
     local lHeros = image:getWidth()
     local hHeros = image:getHeight()
 
-    love.graphics.draw(image,largeur/2,hauteur/2,0,1,1,lHeros/2,hHeros/2)
+    love.graphics.draw(image,imageX,imageY,0,1,1,lHeros/2,hHeros/2)
+
 end
   
 function love.keypressed(key)
