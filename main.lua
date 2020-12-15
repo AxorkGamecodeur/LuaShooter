27449891d2da190730c668c0c93fbc7bf18b128c
axorkgamecodeur <axorkgamecodeur@gmail.com>
2020-12-15 17:30:59 +0100

    Ajout du vaisseau

--- main.lua
+++ main.lua
@@ -8,8 +8,13 @@ love.graphics.setDefaultFilter("nearest")
 -- Cette ligne permet de déboguer pas à pas dans ZeroBraneStudio
 if arg[#arg] == "-debug" then require("mobdebug").start() end
 
+local image
+
 function love.load()
-    
+    largeur = love.graphics.getWidth()
+    hauteur = love.graphics.getHeight()
+
+    image = love.graphics.newImage("images/heros.png")
 end
   
 function love.update(dt)
@@ -17,7 +22,14 @@ function love.update(dt)
 end
   
 function love.draw()
-            
+
+    love.graphics.line(largeur/2, 0, largeur/2, hauteur)
+    love.graphics.line(0, hauteur/2, largeur, hauteur/2)
+
+    local lHeros = image:getWidth()
+    local hHeros = image:getHeight()
+
+    love.graphics.draw(image,largeur/2,hauteur/2,0,1,1,lHeros/2,hHeros/2)
 end
   
 function love.keypressed(key)
